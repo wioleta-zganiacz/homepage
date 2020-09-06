68b22d62763f541dcacb1ef1fea9343c23ee8cbc
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-09-06 13:14:35 +0100

    creating img folder & adding greeting in js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const firstName = 'Wioleta';
 const age = 36 ;
 
-console.log(`Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`);
+console.log(`Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lat.`);
 
+
+
+const greet = (age, firstName) => {
+    console.log(`Witam Cię na mojej stronie. Mam na imię ${firstName}  i mam ${age} lat.`);
+}
+
+greet(36, 'Wioleta');
